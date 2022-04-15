c6aec10f8747a584628b0bdec94f5b7b242d3f7c
jesuanp <jespaterm@gmail.com>
2022-04-14 21:49:49 -0300

    agregando el carousel con las imagenes

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,9 +1,14 @@
-import React from "react";
-// import s from './home.module.css';
+import React, {useState} from "react";
+import s from './home.module.css';
 import IconoChat from '../iconoChat/IconoChat';
 import {useSelector} from 'react-redux';
 import {useNavigate} from 'react-router-dom';
 import Head from '../head/Head';
+import imagenUno from './images/imagen1.jpeg';
+import imagenDos from './images/imagen2.jpeg';
+import imagenTres from './images/imagen3.jpeg';
+import imagenCuatro from './images/imagen4.jpeg';
+import imagenCinco from './images/imagen5.jpeg';
 
 const Home = () => {
 
@@ -11,14 +16,48 @@ const Home = () => {
 
     const usuario = useSelector(state => state.usuario);
 
+    const [posicion, setPosicion] = useState(1);
+
+    const arrayImagenes = [
+        imagenUno,
+        imagenDos,
+        imagenTres,
+        imagenCuatro,
+        imagenCinco
+    ]
+
+    
     React.useEffect(()=>{
-        if(usuario?.bloqueado === 'true') navigate('/no-autorizado');
-    }, [usuario]);
+
+        setInterval(() => {
+            console.log('setInterval')
+            setPosicion(p => p === arrayImagenes.length-1 ? 0 : p + 1);
+    
+        }, 3000)
+    }, []);
 
     return (
         <>
             <Head />
-            {/* <div className={s.fondo}></div> */}
+
+            <div className={s.container}>
+
+                <div className={s.carousel}>
+                    <div>
+                        <img className={s.imgLeft} src={posicion === 0 ? arrayImagenes[arrayImagenes.length-1] : arrayImagenes[posicion-1]} alt="imagen de anuncio" />
+                    </div>
+                    
+                    <div className={s.divImg}>
+                        <img className={s.imgCenter} src={arrayImagenes[posicion]} alt="imagen de anuncio" />
+                    </div>
+                    
+                    <div>
+                        <img className={s.imgRight} src={posicion === arrayImagenes.length-1 ? arrayImagenes[0] : arrayImagenes[posicion+1]} alt="imagen de anuncio" />
+                    </div>
+                </div>
+                
+            </div>
+
             <IconoChat />
         </>
     )
