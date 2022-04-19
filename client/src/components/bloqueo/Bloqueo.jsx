71e0f858f33ab971361f09706aad89e573dfc06d
jesuanp <jespaterm@gmail.com>
2022-04-18 22:32:30 -0300

    Modificando el texto de la seccion se bloqueo y agregando un box shadow al css

--- client/src/components/bloqueo/Bloqueo.jsx
+++ client/src/components/bloqueo/Bloqueo.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import IconoChat from "../iconoChat/IconoChat";
+import {NavLink} from 'react-router-dom';
 import s from './bloqueo.module.css';
 import Head from "../head/Head";
 
@@ -10,7 +11,8 @@ const Bloqueo = () => {
             <Head />
             <div className={s.container}>
                 <div className={s.ventana}>
-                    <span className={s.text}>Informar al administrador que te de acceso a los datos</span>
+                    <span className={s.text}>Escriba un mensaje a soporte y solicite la activación</span>
+                    <NavLink to='/chat' className={s.navLink}>Soporte</NavLink>
                 </div>
             </div>
 
